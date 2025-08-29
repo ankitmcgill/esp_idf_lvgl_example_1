0ffd481f59885259c894108e4dabc261e47ed51d
ankit bhatnagar <ankit.bhatnagarindia@gmail.com>
2025-08-29 11:47:11 +0530

    added mainapp information printing

--- src/projects/mainapp/main/main.c
+++ src/projects/mainapp/main/main.c
@@ -3,6 +3,7 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
 #include "driver_chipinfo.h"
 #include "driver_appinfo.h"
@@ -10,6 +11,8 @@
 
 void app_main(void)
 {
+    ESP_LOGI(DEBUG_TAG_MAINAPP, "");
+    ESP_LOGI(DEBUG_TAG_MAINAPP, "");
     ESP_LOGI(DEBUG_TAG_MAINAPP, "Init");
 
     DRIVER_CHIPINFO_Init();
@@ -26,33 +29,14 @@ void app_main(void)
     size_flash = DRIVER_CHIPINFO_GetFlashSizeBytes();
     size_ram = DRIVER_CHIPINFO_GetRamSizeBytes();
 
-
-    // /* Print chip information */
-    // esp_chip_info_t chip_info;
-    // esp_chip_info(&chip_info);
-    // printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-    //        CONFIG_IDF_TARGET,
-    //        chip_info.cores,
-    //        (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-    //        (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-    //        (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-    //        (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    // unsigned major_rev = chip_info.revision / 100;
-    // unsigned minor_rev = chip_info.revision % 100;
-    // printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-
-
-    // printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-    //        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    // printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    // for (int i = 10; i >= 0; i--) {
-    //     printf("Restarting in %d seconds...\n", i);
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    // }
-    // printf("Restarting now.\n");
-    // fflush(stdout);
-    // esp_restart();
+    ESP_LOGI(DEBUG_TAG_MAINAPP, "**************************");
+    ESP_LOGI(DEBUG_TAG_MAINAPP, "MAC "MACSTR, MAC2STR(c_mac));
+    ESP_LOGI(DEBUG_TAG_MAINAPP, "CHIP INFO: %s %s %s %s",
+        CONFIG_IDF_TARGET,
+        (c_info.features & CHIP_FEATURE_WIFI_BGN) ? "WIFI" : "",
+        (c_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+        (c_info.features & CHIP_FEATURE_BT) ? "BT" : ""
+    );
+    ESP_LOGI(DEBUG_TAG_MAINAPP, "FLASH : %u MB", size_flash/(1024 * 1024));
+    ESP_LOGI(DEBUG_TAG_MAINAPP, "**************************");
 }
