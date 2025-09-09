da8c9ee5bae32cc9b99885a7223d37d452f4d6d1
ankit bhatnagar <ankit.bhatnagarindia@gmail.com>
2025-09-09 18:03:58 +0530

    split defines into global & project defines

---
+++ src/common/others/defines/include/globals.h
@@ -0,0 +1,14 @@
+// DEFINES - GLOBALS
+// SEPTEMBER 9, 2025
+
+#ifndef _DEFINES_GLOBALS_
+#define _DEFINES_GLOBALS_
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+// Event Groups
+extern EventGroupHandle_t handle_event_group_driver_wifi;
+extern EventGroupHandle_t handle_event_group_module_wifi;
+
+#endif
---
+++ src/projects/test_wifi/project_defines/include/project_defines.h
@@ -0,0 +1,14 @@
+// PROJECT DEFINES
+// SEPTEMBER 9, 2025
+
+#ifndef _PROJECT_DEFINES_
+#define _PROJECT_DEFINES_
+
+#define PROJECT_NAME            ("Test_Wifi")
+
+#define DEBUG_TAG_MAIN          ("Main")
+
+#define DEFAULT_WIFI_SSID       ("Ankit_wifi")
+#define DEFAULT_WIFI_PASSWORD   ("casa@2021")
+
+#endif
