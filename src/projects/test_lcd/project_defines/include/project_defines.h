e860582e1dee31667dad3b71dd6ad330e9a95815
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2025-10-01 01:53:35 +0530

    added test_lcd skeleton project

---
+++ src/projects/test_lcd/project_defines/include/project_defines.h
@@ -0,0 +1,11 @@
+// PROJECT DEFINES
+// SEPTEMBER 9, 2025
+
+#ifndef _PROJECT_DEFINES_
+#define _PROJECT_DEFINES_
+
+#define PROJECT_NAME            ("Test_Lcd")
+
+#define DEBUG_TAG_MAIN          ("Main")
+
+#endif
